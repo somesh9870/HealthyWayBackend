634f25bd9f9e554fc945596f4759369c59e84dab
somesh9870 <someshrawat106.com>
2023-04-01 15:58:34 +0530

    changes in user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -152,7 +152,7 @@ userRouter.get("/", async (req, res) => {
   }
 });
 
-userRouter.get("/user/:id", async (req, res) => {
+userRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await UserData.find({ userID: id });
